8b79b92e4cfed0954a63d7e245415a6ae2aa57b6
hoijoii <oodh3328@gmail.com>
2023-01-12 13:45:16 +0900

    First commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="container">
+      <div class="box">
+        <div class="navbar">
+          <ul>
+            <li v-for="tab in tabs" :key="tab.id" class="step">
+              <div class="num">{{ tab.id }}</div>
+              <div class="item">
+                <div class="step-nm">{{ tab.step }}</div>
+                <div class="name">{{ tab.name }}</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="content">
+          content
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+/* eslint-disable */
+const tabs = require('@/assets/data/tabs-info.json')
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
 </script>
