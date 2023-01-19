be56f2521d903db3d482db487958303c9e24a870
hoijoii <oodh3328@gmail.com>
2023-01-19 13:54:47 +0900

    navbar 클릭 기능 삭제

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <div class="box">
         <div class="navbar">
           <ul>
-            <li v-for="tab in tabs" :key="tab.id" class="step" @click="setTabContent(tab.id)">
+            <li v-for="tab in tabs" :key="tab.id" class="step">
               <div :class="['num', {'clicked':tab.id === commonsStore.nowTab}]">{{ tab.id }}</div>
               <div class="item">
                 <div class="step-nm">{{ tab.step }}</div>
